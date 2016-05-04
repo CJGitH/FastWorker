c793ff9683bdef2f26f9ae025487ba938b071d5d
CJGitH <2392777628@qq.com>
2016-05-04 17:44:54 +0800

    初始化项目

---
+++ FastWorker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FastWorker
+//
+//  Created by chen on 16/5/4.
+//  Copyright © 2016年 chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
